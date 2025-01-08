ce58bf4707a202e4222d4a73536fe33837daa481
gers <geraquem@gmail.com>
2025-01-08 19:06:28 +0100

    wip

--- app/src/main/java/com/mmfsin/qep/presentation/main/MainActivity.kt
+++ app/src/main/java/com/mmfsin/qep/presentation/main/MainActivity.kt
@@ -2,17 +2,29 @@ package com.mmfsin.qep.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.mmfsin.qep.databinding.ActivityBedrockBinding
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.mmfsin.qep.R
+import com.mmfsin.qep.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityBedrockBinding
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityBedrockBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        changeStatusBar()
+    }
+
+    private fun changeStatusBar() {
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.isAppearanceLightStatusBars = true
     }
 }
